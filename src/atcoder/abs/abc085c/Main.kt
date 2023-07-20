46e8649050bc9b3a5d9a9cc095d3db035b1474be
elzup <guild0105@gmail.com>
2023-07-20 20:57:01 +0900

    refactor

--- src/atcoder/abs/abc085c/Main.kt
+++ src/atcoder/abs/abc085c/Main.kt
@@ -1,74 +1,20 @@
-typealias Matrix = Array<Array<Int>>
-
 fun main(args: Array<String>) {
 
     fun readInts() = readLine()!!.split(" ").map { it.toInt() }
 
-    fun createLaplaceMarix(matrix: Matrix, n: Int, line: Int, col: Int): Matrix {
+    val (n, price) = readInts()
 
-        val laplace_marix: Matrix = Array(n - 1) { Array(n - 1) { 0 } }
-        var y = 0
-        for (i in 0..n - 1) {
-            if (i == col) continue
-            var x = 0
-            for (j in 0..n - 1) {
-                if (i == line || j == col) continue
-                if (j == line) continue
-                laplace_marix[y][x] = matrix[i][j]
-                x++
-            }
-            y++
-        }
-        return laplace_marix
-    }
-
-    fun dat(matrix: Matrix, n: Int): Int {
-
-        if (matrix.size == 1) return matrix[0][0]
-        if (matrix.size == 2) return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
-        var sum = 0
-        for (j in 0..matrix.size - 1) {
-            val sign = if ((j % 2) == 0) 1 else -1
-            val laplace_marix = createLaplaceMarix(matrix, n, 0, j)
-            sum += sign * matrix[j][0] * dat(laplace_marix, n - 1)
-        }
-        return sum
-    }
-    fun changeColumn(matrix: Matrix, n: Int, col1: Int, col2: Int): Matrix {
-        val tempMat: Matrix = Array(n) { Array(n + 1) { 0 } }
-        for (i in 0..n - 1) {
-            for (j in 0..n) {
-                tempMat[i][j] = matrix[i][j]
-            }
-        }
-
-        for (i in 0..n - 1) {
-            val temp = tempMat[i][col1]
-            tempMat[i][col1] = tempMat[i][col2]
-            tempMat[i][col2] = temp
-        }
-        return tempMat
-    }
-
-    val (n, y) = readInts()
-
-    fun solve(matrix: Matrix, d: Int): Array<Int> {
-        val denominator = dat(matrix, d)
-        val result = Array(d) { 0 }
-        for (i in 0..d - 1) {
-            val numeMat = changeColumn(matrix, d, i, d)
-            result[i] = dat(numeMat, d) / denominator
-        }
-        return result
-    }
-
-    val maxI = y / 10000 + 1
-    for (i in maxI downTo 0) {
-        val matrix = arrayOf(arrayOf(5000, 1000, y - 10000 * i), arrayOf(1, 1, n - i))
-        val (a, b) = solve(matrix, 2)
-        if (a >= 0 && b >= 0 && i * 10000 + a * 5000 + b * 1000 == y && i + a + b == n
+    val maxI = price / 10000 + 1
+    val (a, b, c, d) = listOf(5000, 1000, 1, 1)
+    val deno = a * d - b * c
+    for (x in maxI downTo 0) {
+        val (p, q) = listOf(price - 10000 * x, n - x)
+        val y = (p * d - q * b) / deno
+        val z = (a * q - c * p) / deno
+        // println("$x $y $z ")
+        if (y >= 0 && z >= 0 && x * 10000 + y * 5000 + z * 1000 == price && x + y + z == n
         ) { // 整数でのチェック
-            println("$i $a $b")
+            println("$x $y $z")
             return
         }
     }
